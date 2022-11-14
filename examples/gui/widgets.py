635cfd70f7ab87801f667317684956c096533421
Benjamin Moran <benmoran@protonmail.com>
2022-11-14 09:12:13 +0900

    Update usage of `set_handler` in widgets example.

--- examples/gui/widgets.py
+++ examples/gui/widgets.py
@@ -55,27 +55,27 @@ frame = pyglet.gui.Frame(window, order=4)
 
 
 togglebutton = pyglet.gui.ToggleButton(100, 400, pressed=pressed, depressed=depressed, hover=hover, batch=batch)
-togglebutton.set_handler('on_toggle', toggle_button_handler)
+togglebutton.push_handler('on_toggle', toggle_button_handler)
 frame.add_widget(togglebutton)
 toggle_label = pyglet.text.Label("Toggle Button: False", x=300, y=400, batch=batch, color=(0, 0, 0, 255))
 
 
 pushbutton = pyglet.gui.PushButton(100, 300, pressed=pressed, depressed=depressed, hover=hover, batch=batch)
-pushbutton.set_handler('on_press', push_button_handler)
-pushbutton.set_handler('on_release', release_button_handler)
+pushbutton.push_handler('on_press', push_button_handler)
+pushbutton.push_handler('on_release', release_button_handler)
 frame.add_widget(pushbutton)
 push_label = pyglet.text.Label("Push Button: False", x=300, y=300, batch=batch, color=(0, 0, 0, 255))
 
 
 slider = pyglet.gui.Slider(100, 200, bar, knob, edge=5, batch=batch)
-slider.set_handler('on_change', slider_handler)
+slider.push_handler('on_change', slider_handler)
 frame.add_widget(slider)
 slider_label = pyglet.text.Label("Slider Value: 0.0", x=300, y=200, batch=batch, color=(0, 0, 0, 255))
 
 
 text_entry = pyglet.gui.TextEntry("Enter Your Name", 100, 100, 150, batch=batch)
 frame.add_widget(text_entry)
-text_entry.set_handler('on_commit', text_entry_handler)
+text_entry.push_handler('on_commit', text_entry_handler)
 text_entry_label = pyglet.text.Label("Text: None", x=300, y=100, batch=batch, color=(0, 0, 0, 255))
 
 
